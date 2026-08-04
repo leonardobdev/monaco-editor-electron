785c1ab614136499614fbfbc8700cdb75ab9b102
leonardobdev <138508225+leonardobdev@users.noreply.github.com>
2026-08-04 13:44:46 +0000

    first commit

---
+++ preload.js
@@ -0,0 +1,4 @@
+// Add native IPC bridges here if your editor needs to save/open files locally
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Electron preload script injected.');
+});
